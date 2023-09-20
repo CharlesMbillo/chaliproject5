ba9efdaf0661ccce797b4a7d290e4e3e09c97700
charlesmbillo <mbillocharles@gmail.com>
2023-09-20 23:05:35 +0300

    urlconfig

--- chaliproject5/urls.py
+++ chaliproject5/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,5 +31,5 @@ from django.views.generic.base import RedirectView
 favicon_view = RedirectView.as_view(url='/static/favicon.png', permanent=True)
 
 urlpatterns =[
-        re_path (r'^favicon\.png$', favicon_view),
+        path (r'^favicon\.png$', favicon_view),
     ]
--- chaliwebapp2/views.py
+++ chaliwebapp2/views.py
@@ -9,16 +9,16 @@ from .forms import forms
 def user_form(request):
     if request.method == 'POST':
         # If the form was submitted, process the data
-        form = User(request.POST)
-        if form.is_valid():
+        forms = User(request.POST)
+        if forms.is_valid():
             # Form data is valid, save the user profile
-            form.save()
+            forms.save()
             return redirect('success')  # Redirect to a success page
     else:
         # If it's a GET request, display the empty form
-        form = User()
+        forms = User()
 
-    return render(request, 'user_form.html', {'form': form})
+    return render(request, 'user_form.html', {'forms': forms})
 
 def success(request):
     # You can create a success page template or handle it as needed
